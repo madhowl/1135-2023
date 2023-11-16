687660dab98a294d4c65a0773b3aabc2ae5203c2
MadHowl <madhowl@yandex.ru>
2023-11-16 16:00:07 +1000

    putting things in order

--- template/backend/pages/article-form.php
+++
@@ -1,9 +0,0 @@
-<main id="main" class="main">
-    <section class="section">
-        <div class="row">
-            <div class="col-lg-12">
-                <?php showArticleForm() ?>
-            </div>
-        </div>
-    </section>
-</main><!-- End #main -->
